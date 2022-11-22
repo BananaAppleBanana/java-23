72e7afb117fde7826607d52dee97314f4e68ca49
renqingyang <rq625230882@gmail.com>
2022-11-22 11:54:35 -0600

    evaluation review

--- src/main/java/com/example/java23/week3/EvaluationReview.java
+++ src/main/java/com/example/java23/week3/EvaluationReview.java
@@ -219,12 +219,157 @@ class ThreadPoolTest {
 }
 
 /**
+ *  SOLID
+ *  Single responsibility
+ *  Open - close
+ *  Liskov substitution
+ *  Interface segregation
+ *  Dependency inversion
+ *      class A {
+ *          private B b;
+ *      }
+ *      interface B {}
+ *      class BImpl1 implements B {}
+ *      class BImpl2 implements B {}
+ */
+
+/**
+ *  stack over flow error
+ *  out of memory error
+ *
+ *
+ *     [eden][s0][s1] young gen   minor gc
+ *      old gen     major gc
+ *
+ *      G1 : young gen + old
+ *      [][][][][][][][][]
+ *      [][][][][][][][][]
+ *      [][][][][][][][][]
+ *      [][][][][][][][][]
+ *
+ *      CMS
+ *          1. initial mark root (STW)
+ *          2. concurrent mark
+ *          3. final mark(STW)
+ *          4. concurrent remove
+ *      Mark and sweep + compress (STW)
+ *
+ * 1. heap dump => Jprofiler / java mission control / memory analyzer
+ *    check memory leak
+ * 2. increase young gen size
+ * 3. reference
+ *      SoftReference => heap full -> remove object
+ *      WeakReference =>
+ *      PhantomReference + Reference Queue =>
+ *
+ */
+
+
+/**
  *  sql + db
+ *
+ *  emp m - 1 dept
+ *  emp(id(pk), name, salary, dept_id(fk))
+ *  dept(id(pk), name)
+ *
+ *  2nd highest salary in each department
+ *  display
+ *  id, dept_name, salary
+ *  ..  ..
+ *  select ... t.salary
+ *  from (select e.*, dense_rank() over (partition by d.dept_id order by salary desc) as rank
+ *      from emp e (inner) join dept d e.dept_id = d.id) t
+ *  where t.rank = 2
+ *
+ *  count number of employees in each dept
+ *
+ *  select d.id, count(e.id)
+ *  from dept d left join emp e on d.id = e.dept_id
+ *  group by d.id
+ *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *
+ *   ACID ->
+ *
+ *   id,  name      row_id,  tx_id, rollback pointer
+ *    1    Tom         xx11      1        null
+ *
+ *    begin tx
+ *    update table set name = 'Alex' where id = 1
+ *
+ *
+ *   id,  name      row_id,  tx_id, rollback pointer
+ *    1    Alex        xx22      2        |
+ *                                        |
+ *                                       1    Tom         xx11      1        null
+ *
+ *    rc
+ *    select * => read_view(committed tx id list [1])
+ *    select * => read_view
+ *
+ *    rr
+ *    select * => read_view(committed tx id list [1])
+ *    select * => use previous read_view
+ *
+ *    serializable
+ *     *     *     *     *     *     *     *     *     *     *     *     *     *     *     *
+ *     index
+ *
+ *          [1, 10, 20]
+ *            add(15)
+ *                  [15]
+ *                /     \
+ *          [1, 10]     [15, 20]
+ *
+ *                    [11, 15,  30]
+ *               /     /      \       \
+ *        [1, 10] <->[11, 12] <->[15, 20] <->[30, 40]
+ *        rowid         rowid       rowid       rowid
+ *         name
+ *         ..
+ *
+ *    index organized table (IOT)
+ *     *   *   *   *   *   *   *   *   *   *   *   *   *
+ *     1 ~ Long.MAX_VALUE   => design system => query(val) : return true(exist),  false(not found)
+ *
+ *
+ *     1. hashmap / set => redis cache cluster
+ *     2. database table => primary key + b+ index
+ *     3. bitmap
+ *        101010000100..00000000000000    Long.MAX_VALUE
+ *
+ *    bitmap index
+ *     id  state    NJ      NY      VA
+ *     1     NJ     1       0       0
+ *     2     NY     0       1       0
+ *     3     NJ     1       0       0
+ *     4     VA     0       0       1
+ *
+ *
+ *      NY || VA
+ *     *     *     *     *     *     *     *     *     *
+ *     where id = 5 or id = 6
+ *     where id >= 5 and id <= 6
+ *
+ *     index(table column) / full(table) / leading(t1..) / parallel()
+ *     use_nl / use_hash / use_merge
+ *
+ *     execution plan (statistics)
+ *          index access path:
+ *              index unique scan
+ *              index range scan
+ *              index full scan
+ *              index fast full scan
+ *          full table scan
+ *
+ */
+
+/**
  *  design pattern (singleton, factory...)
- *
  *  design pattern + reflection
- *
- *
+ */
+
+
+
+/**
  *  Next Week
  *      Monday : ORM(hibernate + jpa + spring data jpa) + JDBC
  *      Tuesday : Spring + Spring Boot
---
+++ src/main/java/com/example/java23/week3/pattern/SingletonDemo.java
@@ -0,0 +1,3 @@
+package com.example.java23.week3.pattern;
+
+
